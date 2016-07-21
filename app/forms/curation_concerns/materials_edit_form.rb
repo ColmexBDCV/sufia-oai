55bd2c00396ac3dc028b96367b878e882c1af116
Stephen Cassidy <cassidy.108@cassidy108m1.lib.ohio-state.edu>
2016-07-21 09:57:58 -0400

    add form stuff for corey

--- app/forms/curation_concerns/materials_edit_form.rb
+++ app/forms/curation_concerns/materials_edit_form.rb
@@ -1,9 +1,8 @@
 module CurationConcerns
   module MaterialsEditForm
     extend ActiveSupport::Concern
-
     module ClassMethods
-
+ 
       def build_permitted_params
         permitted = super
         permitted << { :materials_attributes => [:material_type, :material, :id, :_destroy] }
--- app/forms/curation_concerns/measurements_edit_form.rb
+++ app/forms/curation_concerns/measurements_edit_form.rb
@@ -1,17 +1,10 @@
 module CurationConcerns
   module MeasurementsEditForm
     extend ActiveSupport::Concern
-
-    module ClassMethods
-
-      def build_permitted_params
-        permitted = super
-        permitted << { :measurements_attributes => [:measurement, :measurement_unit, :measurement_type, :id, :_destroy] }
-        permitted
-      end
-        
+    
+    included do
+      self.terms += [:measurement_unit, :measurement_type, :measurement] 
     end
-
       # This is required so that fields_for will draw a nested form.
       # See ActionView::Helpers#nested_attributes_association?
       #   https://github.com/rails/rails/blob/a04c0619617118433db6e01b67d5d082eaaa0189/actionview/lib/action_view/helpers/form_helper.rb#L1890
