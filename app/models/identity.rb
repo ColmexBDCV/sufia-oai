af8be2e1ddacc6a419584a3c0751a0b5726f4e0e
Corey Hinshaw <hinshaw.25@osu.edu>
2016-08-03 20:53:18 -0400

    Identity should validate uniqueness correctly

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,7 +2,7 @@ class Identity < ActiveRecord::Base
   belongs_to :user
 
   validates :uid, :provider, presence: true
-  validates :uid, uniqueness: true, scope: :provider
+  validates :uid, uniqueness: { scope: :provider }
 
   class << self
     def from_omniauth(auth)
