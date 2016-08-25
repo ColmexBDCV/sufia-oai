47845af17086aa171858992ae80e6d4e78492306
Corey Hinshaw <hinshaw.25@osu.edu>
2016-08-25 10:55:42 -0400

    Use correct host name for staging deployments

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,12 +1,11 @@
 # Server Roles
 # ==================
 
-role :app,    %w(rails@lib-wit2rub-v02.lib.ohio-state.edu)
-role :web,    %w(rails@lib-wit2rub-v02.lib.ohio-state.edu)
-role :db,     %w(rails@lib-wit2rub-v02.lib.ohio-state.edu)
+role :app,    %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
+role :web,    %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
+role :db,     %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
 
 # Rails Environment
 # ======================
 
 set :rails_env, 'production'
-set :branch, ENV['BRANCH'] || 'staging'
