5994abff22c31ce5d6aa5d21a2d8be4f5370067d
Corey Hinshaw <hinshaw.25@osu.edu>
2016-08-29 14:42:41 -0400

    Consolidate Redis configuration

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-redis_config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0", namespace: 'dcs-sidekiq' }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0", namespace: 'dcs-sidekiq' }
-end
