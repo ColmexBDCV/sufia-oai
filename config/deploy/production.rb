29b32ed17e39b51ee04cedd17464802fd4fd6528
Corey Hinshaw <hinshaw.25@osu.edu>
2016-08-29 15:31:39 -0400

    Symlink paperclip attachments directory into relative root while deploying

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,3 +10,8 @@ role :db,     %w(rails@lib-wit1rub-v02.lib.ohio-state.edu)
 
 set :rails_env, 'production'
 set :assets_prefix, 'dcs/assets'
+set :paperclip_relative_root, 'dcs'
+
+namespace :deploy do
+  before :publishing, 'paperclip:symlink_attachments'
+end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,3 +10,8 @@ role :db,     %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
 
 set :rails_env, 'production'
 set :assets_prefix, 'dcs/assets'
+set :paperclip_relative_root, 'dcs'
+
+namespace :deploy do
+  before :publishing, 'paperclip:symlink_attachments'
+end
