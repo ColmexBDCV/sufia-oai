aa4440e5872b78683bded7dd24d264dfcb89b35a
Corey Hinshaw <hinshaw.25@osu.edu>
2016-08-29 14:13:14 -0400

    Style correction

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-redis_config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+redis_config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0", namespace: 'dcs-sidekiq' }
