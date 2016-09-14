d7e534b3e3832eef29f29a85966bfdc96bdc3b85
Jason Paul Michel <michel.31@osu.edu>
2016-09-14 13:44:13 -0400

    remove console.log

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -2,7 +2,6 @@
     var random_number = Math.floor(Math.random()*(3-1+1)+1);
     var home_header_id = 'home_header_' + random_number;
     $('#home_header').attr("id",home_header_id)
-    console.log(home_header_id)
   };
 
   $(document).on('turbolinks:load', random_banner);
