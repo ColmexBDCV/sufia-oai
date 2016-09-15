f19ba682311a71d5cd88d903b02c060198f7b21e
Chris Bartos <bartos.25@osu.edu>
2016-09-15 14:18:09 -0400

    Rebased with Master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908170233) do
+ActiveRecord::Schema.define(version: 20160908212312) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
