58ce109bbde9323bbf0fe24046616d871954edc0
Jason Paul Michel <michel.31@osu.edu>
2016-09-20 12:00:50 -0400

    testing turbolinks fix

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,4 +1,4 @@
-  var random_banner = function() {
+/*  var random_banner = function() {
     var random_number = Math.floor(Math.random()*(3-1+1)+1);
     var home_header_id = 'home_header_' + random_number;
     $('#home_header').attr("id",home_header_id);
@@ -13,3 +13,4 @@
   };
 
   $(document).on('turbolinks:load', random_banner);
+*/
