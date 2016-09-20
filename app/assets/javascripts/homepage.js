88dbb888367e442b19301350626dab769096c9ca
Jason Paul Michel <michel.31@osu.edu>
2016-09-20 12:14:33 -0400

    test turbolinks issue

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -3,6 +3,11 @@
     var home_header_id = 'home_header_' + random_number;
     $('#home_header').attr("id",home_header_id);
 
+    $('#material-fields').nestedFields();
+    $('#measurement-fields').nestedFields();
+
+    $('.select2-field').select2();
+
     var footer_height = $('footer.footer').height();
     $('body').css('margin-bottom', footer_height);
 
