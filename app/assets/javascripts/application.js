81238ba2ee1b5c16b18dae695237289035b62b9d
Jason Paul Michel <michel.31@osu.edu>
2016-09-20 12:05:36 -0400

    remove test on turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,18 +27,6 @@ var ready = function() {
   $('#measurement-fields').nestedFields();
 
   $('.select2-field').select2();
-
-  var random_number = Math.floor(Math.random()*(3-1+1)+1);
-  var home_header_id = 'home_header_' + random_number;
-  $('#home_header').attr("id",home_header_id);
-
-  var footer_height = $('footer.footer').height();
-  $('body').css('margin-bottom', footer_height);
-
-  $(window).resize(function(){
-    var footer_height = $('footer.footer').height();
-    $('body').css('margin-bottom', footer_height);
-  });
 };
 
 $(document).on('turbolinks:load', ready);
