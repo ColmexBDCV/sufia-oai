882ba16e0d4c40f3f9a4765f51ff5069a5ac987d
Jason Paul Michel <michel.31@osu.edu>
2016-09-28 11:37:23 -0400

    removed turbolinks and added document ready

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,20 +1,10 @@
-  var random_banner = function() {
+$(document).ready(function(){
     var random_number = Math.floor(Math.random()*(3-1+1)+1);
     var home_header_id = 'home_header_' + random_number;
     $('#home_header').attr("id",home_header_id);
-
-    // var footer_height = ($('footer.footer').height() + 30);
-    // $('body').css('margin-bottom', footer_height);
-    //
-    // $(window).resize(function(){
-    //   var footer_height = $('footer.footer').height();
-    //   $('body').css('margin-bottom', footer_height);
-    // });
 
     $('#material-fields').nestedFields();
     $('#measurement-fields').nestedFields();
 
     $('.select2-field').select2();
-  };
-
-  $(document).on('turbolinks:load', random_banner);
+});
