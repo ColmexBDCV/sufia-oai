850c9a4494a8c7823910603cbadb7af098b55635
Corey Hinshaw <hinshaw.25@osu.edu>
2016-09-29 10:03:27 -0400

    Correct field order on generic work form

--- app/forms/curation_concerns/generic_work_form.rb
+++ app/forms/curation_concerns/generic_work_form.rb
@@ -1,15 +1,26 @@
-# Generated via
-#  `rails generate curation_concerns:work GenericWork`
 module CurationConcerns
   class GenericWorkForm < Sufia::Forms::WorkForm
     include MaterialsEditForm
     include MeasurementsEditForm
+
     self.model_class = ::GenericWork
-    self.terms += [:unit, :resource_type, :staff_notes, :sub_collection,
-                   :spatial, :alternative, :temporal, :format, :bibliographic_citation,
-                   :provenance, :work_type, :preservation_level,
-                   :preservation_level_rationale, :handle]
-    self.required_fields += [:unit]
+
+    self.terms = [:resource_type, :title, :unit, :creator, :contributor,
+                  :abstract, :description, :bibliographic_citation, :keyword,
+                  :rights, :provenance, :publisher, :date_created, :subject,
+                  :language, :identifier, :based_near, :related_url, :work_type,
+                  :spatial, :alternative, :temporal, :format, :staff_notes,
+                  :source, :sub_collection, :preservation_level_rationale,
+                  :preservation_level, :collection_identifier,
+                  :measurements_attributes, :materials_attributes,
+                  :representative_id, :thumbnail_id, :files,
+                  :visibility_during_embargo, :embargo_release_date,
+                  :visibility_after_embargo, :visibility_during_lease,
+                  :lease_expiration_date, :visibility_after_lease, :visibility,
+                  :ordered_member_ids, :in_works_ids, :collection_ids]
+
+    self.required_fields = [:resource_type, :title, :unit]
+
     def self.reflect_on_association(association)
       ::GenericWork.reflect_on_association(association)
     end
