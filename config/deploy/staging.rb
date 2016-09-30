772f537114c54c603ffddd2a6b3c6c31e058816e
Corey Hinshaw <hinshaw.25@osu.edu>
2016-09-30 15:18:32 -0400

    Add worker VM to staging deployment configuration

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
 # Server Roles
 # ==================
 
-role :app,    %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
 role :web,    %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
+role :app,    %w(rails@lib-wit2rub-v04.lib.ohio-state.edu)
 role :db,     %w(rails@lib-wit2rub-v03.lib.ohio-state.edu)
 
 # Rails Environment
