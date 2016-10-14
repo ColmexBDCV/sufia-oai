0bfb43767e45385597a0e72f63e2ac42f4b55943
Corey Hinshaw <hinshaw.25@osu.edu>
2016-10-14 16:08:33 -0400

    Add test for title_for method in application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe ApplicationHelper do
       expect(helper.login_strategy).to be :developer
     end
   end
+
+  describe 'title_for' do
+    context 'a model with a name' do
+      let(:model) { build(:import, name: 'My Import') }
+
+      it 'returns the model name' do
+        expect(helper.title_for(model)).to eq model.name
+      end
+    end
+
+    context 'a model with no name' do
+      let(:model) { build(:import, name: nil) }
+
+      it 'returns "Untitled"' do
+        expect(helper.title_for(model)).to eq 'Untitled'
+      end
+    end
+
+    context 'with a property other than name' do
+      let(:model) { build(:user) }
+
+      it 'returns the value of that property' do
+        expect(helper.title_for(model, :email)).to eq model.email
+      end
+    end
+  end
 end
