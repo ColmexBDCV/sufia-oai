790465e1e21ae2c6d263870b5aad2d7440ca649a
Corey Hinshaw <hinshaw.25@osu.edu>
2016-10-19 21:56:41 -0400

    Add featured collection spec

--- app/models/featured_collection.rb
+++ app/models/featured_collection.rb
@@ -1,14 +1,10 @@
 class FeaturedCollection < ActiveRecord::Base
   FEATURE_LIMIT = 5
+
   validate :count_within_limit, on: :create
   validates :order, inclusion: { in: proc { 0..FEATURE_LIMIT } }
 
   default_scope { order(:order) }
-
-  def count_within_limit
-    return if FeaturedCollection.can_create_another?
-    errors.add(:base, "Limited to #{FEATURE_LIMIT} featured works.")
-  end
 
   attr_accessor :presenter
 
@@ -16,5 +12,12 @@ class FeaturedCollection < ActiveRecord::Base
     def can_create_another?
       FeaturedCollection.count < FEATURE_LIMIT
     end
+  end
+
+  private
+
+  def count_within_limit
+    return if FeaturedCollection.can_create_another?
+    errors.add(:base, "Limited to #{FEATURE_LIMIT} featured works.")
   end
 end
