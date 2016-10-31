2d7dd7a6d35ff347f30a8a9beea67de7c091f945
Corey Hinshaw <hinshaw.25@osu.edu>
2016-10-31 13:01:12 -0400

    Do not allow non-admins to set the admin flag on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
   protected
 
   def user_params
-    params.require(:user).permit(:avatar, :facebook_handle, :twitter_handle,
-                                 :googleplus_handle, :linkedin_handle, :remove_avatar, :orcid, :admin)
+    permitted_params = [:avatar, :facebook_handle, :twitter_handle,
+                        :googleplus_handle, :linkedin_handle, :remove_avatar,
+                        :orcid]
+
+    # Only admins can make a user an admin
+    permitted_params.concat([:admin]) if current_user.admin?
+
+    params.require(:user).permit(permitted_params)
   end
 end
