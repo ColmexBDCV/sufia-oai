e07adb1f8fa774812db69ea3092513b2e1d8641e
Corey Hinshaw <hinshaw.25@osu.edu>
2016-10-31 10:30:42 -0400

    Add 1rub-v05 to production deployment configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ==================
 
 role :web,    %w(rails@lib-wit1rub-v03.lib.ohio-state.edu)
-role :app,    %w(rails@lib-wit1rub-v04.lib.ohio-state.edu)
+role :app,    %w(rails@lib-wit1rub-v04.lib.ohio-state.edu rails@lib-wit1rub-v05.lib.ohio-state.edu)
 role :db,     %w(rails@lib-wit1rub-v03.lib.ohio-state.edu)
 
 # Rails Environment
