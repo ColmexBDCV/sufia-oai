28d582d348c3728201d8055a2caa16a234a90d7c
Corey Hinshaw <hinshaw.25@osu.edu>
2016-11-11 11:06:51 -0500

    Precompile assets before CI Rspec tests

--- spec/support/browser.rb
+++ spec/support/browser.rb
@@ -3,7 +3,7 @@ Capybara.default_max_wait_time = 30
 
 Capybara.register_driver :poltergeist do |app|
   config = {
-    timeout: 1.minute,
+    timeout: 60,
     phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'],
     url_blacklist: ['https://ssl.google-analytics.com', 'https://www.osu.edu']
   }
