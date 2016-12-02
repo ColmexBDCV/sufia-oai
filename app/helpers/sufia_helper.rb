185e030b7555511ac6d84febf6a41a0dc808f842
Corey Hinshaw <hinshaw.25@osu.edu>
2016-12-02 13:44:45 -0500

    Make managed works tab the default

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -2,4 +2,13 @@ module SufiaHelper
   include ::BlacklightHelper
   include Sufia::BlacklightOverride
   include Sufia::SufiaHelperBehavior
+
+  def search_action_for_dashboard
+    case params[:controller]
+    when "my/units"
+      sufia.dashboard_units_path
+    else
+      super
+    end
+  end
 end
