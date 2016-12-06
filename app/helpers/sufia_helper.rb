8998ebab78d023dfece0f3a3fd4effa9f22fa0ff
Chris Bartos <bartos.25@osu.edu>
2016-12-06 16:01:26 -0500

    Backout of depositor link fix because it breaks import jobs

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -11,20 +11,4 @@ module SufiaHelper
       super
     end
   end
-
-  # *Sometimes* a Blacklight index field helper_method
-  # @param [String,User,Hash{Symbol=>Array}] args if a hash, the user_key must be under :value
-  # @return [ActiveSupport::SafeBuffer] the html_safe link
-  def link_to_profile(args)
-    user_or_key = args.is_a?(Hash) ? args[:value].first : args
-    user = case user_or_key
-           when User
-             user_or_key
-           when String
-             ::User.find_by_user_key(user_or_key)
-           end
-    return user_or_key if user.nil?
-    text = user.respond_to?(:name) ? user.name : user_or_key
-    link_to text, sufia.profile_path(user)
-  end
 end
