79ce06ef0d909a7ca430e9589f4d944369cf18bd
Chris Bartos <bartos.25@osu.edu>
2017-02-06 15:09:37 -0500

    Updated spec

--- spec/models/import_field_mapping_spec.rb
+++ spec/models/import_field_mapping_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe ImportFieldMapping, type: :model do
     let(:import) { create(:import) }
 
     it 'creates mappings for an import for each field key' do
-      expect {described_class.initiate_mappings(import)}.to change {import.import_field_mappings.count}.from(0).to(31)
+      expect {described_class.initiate_mappings(import)}.to change {import.import_field_mappings.count}.from(0).to(32)
     end
   end
 end
