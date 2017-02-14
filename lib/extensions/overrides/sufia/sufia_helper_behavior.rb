6d1c742584958065792c7c1567d13ac78c420601
Stephen Cassidy <cassidy.108@cassidy108m1.lib.ohio-state.edu>
2017-02-14 15:02:45 -0500

    fix some stuff causing things to fail

--- lib/extensions/overrides/sufia/sufia_helper_behavior.rb
+++ lib/extensions/overrides/sufia/sufia_helper_behavior.rb
@@ -5,8 +5,16 @@ module Overrides
 
       included do
         def link_to_profile(args)
-          super
-          link_to text, ENV['RAILS_RELATIVE_URL_ROOT'].to_s + Sufia::Engine.routes.url_helpers.profile_path(user)
+          user_or_key = args.is_a?(Hash) ? args[:value].first : args
+          user = case user_or_key
+                 when User
+                   user_or_key
+                 when String
+                   ::User.find_by_user_key(user_or_key)
+                 end
+          return user_or_key if user.nil?
+          text = user.respond_to?(:name) ? user.name : user_or_key
+          link_to text, ENV['RAILS_RELATIVE_URL_ROOT'].to_s + sufia.routes.url_helpers.profile_path(user)
         end
       end
     end
--- spec/features/works/work_show_spec.rb
+++ spec/features/works/work_show_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature 'Work show' do
-    let(:generic_work) { create(:public_generic_work, :with_image, :without_validations) }
+  let(:generic_work) { create(:public_generic_work, :with_image, :without_validations) }
 
-    scenario do
-      visit curation_concerns_generic_work_path generic_work
-      expect(page).to have_content 'An image'
-    end
+  scenario do
+    visit curation_concerns_generic_work_path generic_work
+    expect(page).to have_content 'An image'
+  end
 end
