186737fa9b7b5f5b28b9b3a33698f7a0143adbb5
Jennifer Lindner <jenlindner@gmail.com>
2017-10-14 11:06:44 -0500

    production image path in csv and disable auth on works

--- app/controllers/curation_concerns/generic_works_controller.rb
+++ app/controllers/curation_concerns/generic_works_controller.rb
@@ -10,6 +10,30 @@ module CurationConcerns
     before_action :authorize_unit, only: [:create, :update]
     before_action :disable_turbolinks, only: [:edit, :new]
 
+    def show
+      respond_to do |wants|
+        wants.html { presenter && parent_presenter }
+        wants.json do
+          # load and authorize @curation_concern manually because it's skipped for html
+          # This has to use #find instead of #load_instance_from_solr because
+          # we want to return values like file_set_ids in the json
+          @curation_concern = _curation_concern_type.find(params[:id]) unless curation_concern
+          # authorize! :show, @curation_concern
+          render :show, status: :ok
+        end
+        additional_response_formats(wants)
+        wants.ttl do
+          render text: presenter.export_as_ttl
+        end
+        wants.jsonld do
+          render text: presenter.export_as_jsonld
+        end
+        wants.nt do
+          render text: presenter.export_as_nt
+        end
+      end
+    end
+
     private
 
     def authorize_unit
