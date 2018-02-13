39af0bb5da8b23030e04bf564e0850ab4a190b38
Rodrigo Cuéllar Hidalgo <rod.youkai@gmail.com>
2018-02-13 15:08:54 -0600

    changes...

--- app/services/assignor_service.rb
+++ app/services/assignor_service.rb
@@ -1,6 +1,29 @@
 module AssignorService
   mattr_accessor :authority
 
+  def self.check
+    data = []
+    # ActiveFedora::Base.where('has_model_ssim:GenericWork -creator_conacyt_tesim:["" TO *]').each do |g|
+    GenericWork.where('idpersona_isim:[* TO *]-idpersona_isim:0').each do |g|
+
+      d = {}
+      d['titulo'] = g.title
+      d['unidad'] = g.unit
+      if g.file_set_ids.any?
+        idfile = g.file_set_ids[0]
+        d['archivo'] = FileSet.find(idfile).label
+      end
+      d['autor'] = g.creator
+      d['identificador'] = g.identifier
+      d['url'] = "http://repositorio.colmex.mx/concern/generic_works/#{g.id}"
+      d['autor_conacyt'] = g.creator_conacyt
+
+      data.push(d)
+
+    end
+    puts data.to_json
+  end
+
   def self.assign
 
     num = 1
